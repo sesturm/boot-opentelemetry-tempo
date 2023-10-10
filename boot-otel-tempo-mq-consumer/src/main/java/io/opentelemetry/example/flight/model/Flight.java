b863e57ab9a2fabb941df7a605e4956b627dd3e8
Sebastian Sturm <Sebastian.Sturm@doubleSlash.de>
2023-10-10 16:57:11 +0200

    Added Lombok

--- boot-otel-tempo-mq-consumer/src/main/java/io/opentelemetry/example/flight/model/Flight.java
+++ boot-otel-tempo-mq-consumer/src/main/java/io/opentelemetry/example/flight/model/Flight.java
@@ -1,8 +1,11 @@
 package io.opentelemetry.example.flight.model;
 
+import lombok.Data;
+
 import java.util.Date;
 
-public class Flight {
+
+public @Data class Flight {
 
 	private String origin;
 	private String destination;
@@ -10,41 +13,4 @@ public class Flight {
 	
 	private Date departureTime;
 
-	public String getOrigin() {
-		return origin;
-	}
-
-	public void setOrigin(String origin) {
-		this.origin = origin;
-	}
-
-	public String getDestination() {
-		return destination;
-	}
-
-	public void setDestination(String destination) {
-		this.destination = destination;
-	}
-
-	public String getAirline() {
-		return airline;
-	}
-
-	public void setAirline(String airline) {
-		this.airline = airline;
-	}
-
-	public Date getDepartureTime() {
-		return departureTime;
-	}
-
-	public void setDepartureTime(Date departureTime) {
-		this.departureTime = departureTime;
-	}
-
-	@Override
-	public String toString() {
-		return "Flight [origin=" + origin + ", destination=" + destination + ", airline=" + airline + ", departureTime="
-				+ departureTime + "]";
-	}	
 }
--- boot-otel-tempo-provider1/src/main/java/io/opentelemetry/example/flight/repository/entity/Flight.java
+++ boot-otel-tempo-provider1/src/main/java/io/opentelemetry/example/flight/repository/entity/Flight.java
@@ -1,5 +1,7 @@
 package io.opentelemetry.example.flight.repository.entity;
 
+import lombok.Data;
+
 import java.util.Date;
 
 import javax.persistence.Column;
@@ -11,7 +13,7 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "flight")
-public class Flight {
+public @Data class Flight {
 	
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -23,42 +25,4 @@ public class Flight {
 	
 	@Column(name = "departing")
 	private Date departureTime;
-
-	public String getOrigin() {
-		return origin;
-	}
-
-	public void setOrigin(String origin) {
-		this.origin = origin;
-	}
-
-	public String getDestination() {
-		return destination;
-	}
-
-	public void setDestination(String destination) {
-		this.destination = destination;
-	}
-
-	public String getAirline() {
-		return airline;
-	}
-
-	public void setAirline(String airline) {
-		this.airline = airline;
-	}
-
-	public Date getDepartureTime() {
-		return departureTime;
-	}
-
-	public void setDepartureTime(Date departureTime) {
-		this.departureTime = departureTime;
-	}
-
-	@Override
-	public String toString() {
-		return "Flight [origin=" + origin + ", destination=" + destination + ", airline=" + airline + ", departureTime="
-				+ departureTime + "]";
-	}	
 }
